cd6b039c8d8f97f9964196b1cf23c9a8a9d3c62b
SarikViper <dzamolovsardor5@gmail.com>
2024-12-15 19:13:02 +0500

    first commit

--- src/components/Catologue.vue
+++ src/components/Catologue.vue
@@ -1,14 +1,4 @@
-<template>
-    <div class="container">
-        <div class="casual">
-        </div>
-        <div class="elegant"> 
-        </div>
-    </div>
-</template>
 
-<script>
-export default{
     
 }
 </script>
