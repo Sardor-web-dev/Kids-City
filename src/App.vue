6a3ffd9caec983de3486236e63639817b519f6bb
SarikViper <dzamolovsardor5@gmail.com>
2024-12-15 18:50:28 +0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -270,19 +270,7 @@ img{
     gap: 45px;
     align-items: center;
     justify-content: center;
-}
-.fact1{
-    display: flex;
-    flex-direction: column;
-    gap: 15px;
-    p{
-        font-size: 20px;
-    }
-    span{
-        font-size: 17px;
-    }
-}
-.fact2{
+a
     display: flex;
     flex-direction: column;
     gap: 15px;
