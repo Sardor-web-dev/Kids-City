30ec48aec2c36838a0b3752ad7629d68b7006263
SarikViper <dzamolovsardor5@gmail.com>
2024-12-21 23:10:29 +0500

    firts commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,26 @@
-import { createApp } from 'vue'
+import { createApp, openBlock } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { jsx } from 'vue/jsx-runtime'
 
 createApp(App).mount('#app')
 
 
+
+lj
+length
+jsxj
+jsx
+'pih
+
+j[PromiseRejectionEvent
+    
+    [onrejectionhandled
+        'pih
+        
+    ]PromiseRejectionEvent
+    openBlock
+    'kn';,m/m 
+    
+    ;kjlb
+    '
